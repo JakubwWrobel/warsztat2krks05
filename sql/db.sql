751fa86db81535fb44ac5215ac35c693f60ba0bf
JakubwWrobel <36984485+JakubwWrobel@users.noreply.github.com>
2021-03-17 18:40:55 +0100

    Update db.sql

--- sql/db.sql
+++ sql/db.sql
@@ -10,3 +10,32 @@ CREATE TABLE users (
   PRIMARY KEY (id)
 );
 
+ public static String reversePattern(String text, String pattern) {
+        char[] textChars = text.toCharArray();
+        boolean flag = false;
+        int whereItStarts = 0;
+        for (int i = 0; i < textChars.length; i++) {
+            if (text.charAt(i) == pattern.charAt(0)) {
+                whereItStarts = i;
+                for (int j = 1; j < pattern.length(); j++) {
+                    if (text.charAt(i + j) == pattern.charAt(j)) {
+                        flag = true;
+                    } else {
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (flag) {
+            for (int i = 0; i <= pattern.length() - 1; i++) {
+                textChars[whereItStarts] = pattern.charAt(pattern.length() - i - 1);
+                whereItStarts++;
+            }
+            return textChars.toString();
+        }else {
+            return "Pattern has not been found";
+        }
+    }
+}
